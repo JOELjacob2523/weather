b94c3c2e419f77d6e845f2152f2d9f8340b43147
JOELjacob2523 <Shlomejacob@gmail.com>
2023-04-19 22:12:10 -0400

    first commit

---
+++ api/Program.cs
@@ -0,0 +1,20 @@
+using Api.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("appsettings.json");
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/{zipcode}", (string zipcode) =>
+{
+
+    var apiKey = app.Configuration["Weather:ApiKey"]!;
+
+    var client = new WeatherApiClient(apiKey);
+
+    return client.GetWeather(zipcode);
+});
+
+app.Run();
